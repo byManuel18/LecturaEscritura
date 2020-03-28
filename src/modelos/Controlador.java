880fae16599cf778b2df9dc98c13d7c4e0a2a5be
Manueh <Manueh@DESKTOP-ST5PGCD>
2020-03-28 19:29:19 +0100

    Mostrar Carpeta hecho

--- src/modelos/Controlador.java
+++ src/modelos/Controlador.java
@@ -5,12 +5,78 @@
  */
 package modelos;
 
+import utilidades.Utilities;
+import java.io.*;
+
 /**
  *
  * @author Manueh
  */
 public class Controlador {
-    public static void run(){
+private static File mF;
+    public static void run() {
+        Menu();
+    }
+
+    private static void Menu() {
+        int op = 0;
+        do {
+            op = Utilities.Menu();
+            ControladorMenu(op);
+        } while (op != 12);
+
+    }
+
+    private static void ControladorMenu(int opcion) {
+
+        switch (opcion) {
+            case 1:
+                MostrarCarpeta();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                Utilities.P("Gracias por usar nuestro gestor.");
+                break;
+            default:
+                Utilities.P("Opción no válida.");
+        }
+
+    }
+    
+    private static void MostrarCarpeta(){
+        mF=new File(Utilities.getString("Introduce la dirección de la carpeta"));
+        String[] cosas=mF.list();
         
+        if(cosas!=null){
+            if(cosas.length==0){
+            Utilities.P("Carpeta vacía.");
+        }else{
+            for(String s:cosas){
+                Utilities.P(s);
+            }
+        }
+        }else{
+            Utilities.P("No existe la carpeta.");
+        }
+
     }
 }
